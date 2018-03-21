d0cc8a97acdb3746016481765675314aa21b563c
TropicalCyborg <tropicalcyborg@gmail.com>
2018-03-21 19:22:08 -0300

    refinando definicao intent

--- app.js
+++ app.js
@@ -53,8 +53,8 @@ var intents = new builder.IntentDialog({ recognizers: [recognizer] })
     })
     .matches('Definicao', (session) => {
       //  var entityChoices = ["HMD.Daydream", "HMD.cardboard","HMD.gear","HMD.htc","HMD.rift","HMD.magicleap","HMD.hololens"];
-      //  var entity = builder.EntityRecognizer.findBestMatch(entityChoices);
-        session.send('You reached **Definicao** intent, you said **\'%s\'**.', session.message.text);
+        var entity = builder.EntityRecognizer.findEntity(intents.entities, "HMD");
+        session.send('You reached **Definicao** intent, you said **\'%s\'**.'+ entity, session.message.text);
     })
     .onDefault((session) => {
         session.send('Pouz, não entendi o que vc quis dizer com: **\'%s\'**.', session.message.text);
