37a6833220582ddf9d68cce1d8a28abd4eb4ab70
TropicalCyborg <tropicalcyborg@gmail.com>
2018-03-21 13:49:25 -0300

    removendo tudo menos proatividade

--- app.js
+++ app.js
@@ -28,11 +28,6 @@ var tableName = 'botdata';
 var azureTableClient = new botbuilder_azure.AzureTableClient(tableName, process.env['AzureWebJobsStorage']);
 var tableStorage = new botbuilder_azure.AzureBotStorage({ gzipData: false }, azureTableClient);
 
-var luisAppId = process.env.LuisAppId;
-var luisAPIKey = process.env.LuisAPIKey;
-var luisAPIHostName = process.env.LuisAPIHostName || 'westus.api.cognitive.microsoft.com';
-const LuisModelUrl = 'https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/f1c2986b-e9b0-426f-99ec-faf01acc68b5?subscription-key=69847929754f4440b23d38f8e6b370ec&verbose=true&timezoneOffset=0&q=';
-
 
 // Create your bot with a function to receive messages from the user
 const bot = new builder.UniversalBot(connector);
@@ -63,121 +58,6 @@ bot.on('conversationUpdate',(update) => {
         }
     }
 ]);
-
-/// Luis part of the Code ////
-
-// var recognizer = new builder.LuisRecognizer(LuisModelUrl);
-
-// //var entity = builder.EntityRecognizer.findEntity(args.intent.entities);
-
-// var intents = new builder.IntentDialog({ recognizers: [recognizer] })
-
-//     .matches('Cumprimento', (session) => {
-//         session.send('You reached **Cumprimento** intent, you said **\'%s\'**.', session.message.text);
-        
-//     })
-//     .matches('Xingamento', (session) => {
-//         session.send('You reached **Xingamento** intent,  you said **\'%s\'**.', session.message.text);
-//     })
-//     .matches('Definicao', (session) => {
-//         var entityChoices = ["HMD.Daydream", "HMD.cardboard","HMD.gear","HMD.htc","HMD.rift","HMD.magicleap","HMD.hololens"];
-//         var entity = builder.EntityRecognizer.findBestMatch(entityChoices);
-//         session.send('You reached **Definicao** intent, you said **\'%s\'**.', session.message.text);
-//     })
-//     .onDefault((session) => {
-//         session.send('Pouz, não entendi o que vc quis dizer com: **\'%s\'**.', session.message.text);
-//         // goToQnA(session);
-// });
-
-// bot.dialog('/', intents);
-
-
-////End of Luis Part of the Code ////
-
-
-////QnAMaker part of the Code //////
-
-// var goToQnA = (session) =>{
-//     var recognizer = new builder_cognitiveservices.QnAMakerRecognizer({
-//         knowledgeBaseId: process.env.QnAKnowledgebaseId, 
-//         subscriptionKey: process.env.QnASubscriptionKey,
-//         top:3});
-    
-//     var qnaMakerTools = new builder_cognitiveservices.QnAMakerTools();
-//     var qnaMakerTools = new minha.BrazilianQnaMakerTools();//
-//     bot.library(qnaMakerTools.createLibrary());
-    
-    
-//     const qnaMakerDialog = new builder_cognitiveservices.QnAMakerDialog(
-//         {
-//             recognizers: [recognizer],
-//             defaultMessage:'Ops!...Não entendi. Pode reformular a pergunta?',
-//             qnaThreshold: 0.3,
-//             feedbackLib: qnaMakerTools
-//         }
-//     );
-    
-    
-//     qnaMakerDialog.respondFromQnAMakerResult = (session,result) => {
-//         const resposta = result.answers[0].answer;
-//         const partesDaResposta = resposta.split('%');
-//         const [titulo, imagem, descricao, url] = partesDaResposta;
-    
-//         var card4 = ()=>{
-//             const card  = new builder.HeroCard(session)
-//                 .title(titulo)
-//                 .images([builder.CardImage.create(session,imagem.trim())])
-//                 .text(descricao)
-//                 .buttons([ builder.CardAction.openUrl(session, url.trim(), 'mande um email')]);
-//             const retorno = new builder.Message(session).addAttachment(card);
-//             session.send(retorno);
-//         };
-    
-//         var card3 = ()=>{
-//             const card  = new builder.HeroCard(session)
-//                 .title(titulo)
-//                 .images([builder.CardImage.create(session,imagem.trim())])
-//                 .text(descricao);
-//             const retorno = new builder.Message(session).addAttachment(card);
-//             session.send(retorno);
-//         };
-    
-//         var card2 = ()=>{
-//             const card  = new builder.HeroCard(session)
-//             .text(descricao)
-//             .buttons([ builder.CardAction.openUrl(session, url.trim(), 'mande um email')]);
-//             const retorno = new builder.Message(session).addAttachment(card);
-//             session.send(retorno);
-//         };
-    
-//         switch(partesDaResposta.length){
-//             case 4:
-//             card4();
-//             break;
-    
-//             case 3:
-//             card3();
-//             break;
-    
-//             case 2:
-//             card2();
-//             break;
-    
-//             case 1:
-//             session.send(resposta);
-//             break;
-//         }
-//     };
-    
-//     bot.dialog('/', qnaMakerDialog);
- 
-// };
-
-
-
-
-////End of QnAMaker part of the Code //////
-
 
 
 
