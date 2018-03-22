3024b56f77ce07ff775d5b7e7a6c88b464664cc8
TropicalCyborg <tropicalcyborg@gmail.com>
2018-03-22 13:29:58 -0300

    tentando encontrar tipo das entidades

--- app.js
+++ app.js
@@ -82,8 +82,9 @@ var intents = new builder.IntentDialog({ recognizers: [recognizer] })
         });
 
         const allEntities = foundEntities.map(m=>m.entity).join(',');
+        const myEntities = foundEntities.map(me=>me.entity);
         const message01 = allEntities + ' do tipo:' + String(intentType);
-        const message = foundEntities[0] + 'do grupo' + foundEntities[1];
+        const message = myEntities[0] + 'do grupo' + myEntities[1];
 
         session.send('Desobri a intenção **Definicao**, você disse **\'%s\'** e achei as entidades **'+ message +'\**' , session.message.text);
     })
