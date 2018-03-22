199f9265d777a0b8cc7e600ac5d5e7076c3841a9
TropicalCyborg <tropicalcyborg@gmail.com>
2018-03-22 14:07:08 -0300

    tentando extrair entidades do array

--- app.js
+++ app.js
@@ -83,7 +83,7 @@ var intents = new builder.IntentDialog({ recognizers: [recognizer] })
 
         const allEntities = foundEntities.map(m=>m.entity).join(',');
         const message01 = allEntities;
-        const message = foundEntities[0].entity;
+        const message = foundEntities[0].entity + ' entre ' + foundEntities.length + 'entidades';
 
         session.send('Desobri a intenção **Definicao**, você disse **\'%s\'** e achei as entidades **'+ message +'\**' , session.message.text);
     })
