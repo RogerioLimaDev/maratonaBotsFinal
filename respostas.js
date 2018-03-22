c9a31f1fccef3097b0c9cc96cf926fcaf7234996
TropicalCyborg <tropicalcyborg@gmail.com>
2018-03-22 16:25:50 -0300

    fazendo upload de respostas.js

---
+++ respostas.js
@@ -0,0 +1,54 @@
+/*jshint esversion: 6 */
+
+var selecOp = 10;
+var selected = Math.random();
+var ranOp = 0;
+
+//function Respostas(intent, entity){
+function Respostas(string,sessionMessage){
+
+var textoMsg = sessionMessage;
+
+var xingamento = [
+        "Epa! Olha a boca, rapaz!...",
+        "Sua mãe não te deu educação, não?",
+        "Seu IP foi registrado. Vou mandar o Robocop aí te dar uma lição."
+        ];
+
+var Default = [
+        
+        'desculpe, não entendi',
+        'Você pode repetir a pergunta? Não entendi o que vc quis dizer com' + sessionMessage+'.' ,
+        'não entendi...foi mal'
+        //' Poutz! Não entendi o que vc quis dizer com **'+ message +'\**',
+        // 'Eita... agora vc me pegou. Ainda não aprendi como responder a **'+ message +'\** '
+        ];
+
+    switch(string){
+
+        case 'xingamento':
+            ranOp = SelectRandomNumber();
+            return(xingamento[ranOp]);
+
+        case 'None' :
+            ranOp = SelectRandomNumber();
+            return(Default[ranOp]);
+    }
+}
+
+
+function SelectRandomNumber(){
+
+    var teste = 'testando';
+
+    if (selected <= 0.33){ selecOp = 0;}
+    else if (selected <= 0.66 && selected >0.33){ selecOp = 1.0;}
+    else { selecOp = 2.0;}
+
+    return(selecOp);
+
+}
+
+//module.exports.Respostas();
+exports.SelectRandomNumber = SelectRandomNumber;
+exports.Respostas = Respostas;
