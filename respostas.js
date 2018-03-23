63aeeb71da68d6e6e5a79b67af37f5bc0a47bd3a
TropicalCyborg <tropicalcyborg@gmail.com>
2018-03-23 19:18:19 -0300

    revertendo alteração

--- respostas.js
+++ respostas.js
@@ -12,19 +12,41 @@ function Respostas(string,sessionMessage, entity, tipo){
     curEntity = String(entity);
     curType = String(tipo);
 
+    var cumprimento = [
+
+        "Opa! Beleza? O que quer saber sobre a Tropical Cyborg?",
+        "Oi! Sou o Tropical Cyborg e estou aqui para tirar suas dúvidas de Realidade Virtual e aumentada",
+        "E aê? Como posso te ajudar"
+    ];
+    
+    var xingamento = [
+            "**" + textoMsg + "\**??? Epa! Olha a boca, rapaz!...",
+            "**" + textoMsg + "\**??? Sua mãe não te deu educação, não?",
+            "**" + textoMsg + "\**??? Seu IP foi registrado. Vou mandar o Robocop aí te dar uma lição."
+            ];
+    
+    var Default = [
+            
+            'Não entendi o que vc quis dizer com **' + textoMsg +'\** . Minha especialidade é responder sobre realidade aumentada e virtual' ,
+            ' Poutz! Não entendi o que vc quis dizer com **'+ textoMsg +'\**',
+            'Eita... agora vc me pegou. Ainda não aprendi como responder a **'+ textoMsg +'\** '
+            ];
+
+
+
     switch(string){
 
         case 'xingamento':
             ranOp = SelectRandomNumber();
-            return(textos.xingamento[ranOp]);
+            return(xingamento[ranOp]);
 
         case 'cumprimento':
             ranOp = SelectRandomNumber();
-            return(textos.cumprimento[ranOp]);
+            return(cumprimento[ranOp]);
 
         case 'None' :
             ranOp = SelectRandomNumber();
-            return(textos.Default[ranOp]);
+            return(Default[ranOp]);
 
         case 'definicao':
             if(curType != null)
--- respostasTexts.js
+++ respostasTexts.js
@@ -1,24 +1,27 @@
-var cumprimento = {
 
-    1: "Opa! Beleza? O que quer saber sobre a Tropical Cyborg?",
-    2: "Oi! Sou o Tropical CyBot e estou aqui para tirar suas dúvidas de Realidade Virtual e aumentada",
-    3:"E aê? Como posso te ajudar"
-};
-
-var xingamento = {
-    1: "**" + textoMsg + "\**??? Epa! Olha essa boca!...",
-    2: "**" + textoMsg + "\**??? Você não tem educação, não?",
-    3: "**" + textoMsg + "\**??? Seu IP foi registrado. Vou mandar meu primo Robocop aí te dar uma lição."
-};
-
-var Default = {
-        
-    1: 'Não entendi o que vc quis dizer com **' + textoMsg +'\** . Minha especialidade é responder sobre realidade aumentada e virtual' ,
-    2: 'Poutz! Não entendi o que vc quis dizer com **'+ textoMsg +'\**',
-    3: 'Eita... agora vc me pegou. Ainda não aprendi como responder a **'+ textoMsg +'\** '
-};
+function Textos (){
 
 
-module.exports = cumprimento;
-module.exports = xingamento;
-module.exports = Default;
+    var cumprimento = [
+
+        "Opa! Beleza? O que quer saber sobre a Tropical Cyborg?",
+        "Oi! Sou o Tropical Cyborg e estou aqui para tirar suas dúvidas de Realidade Virtual e aumentada",
+        "E aê? Como posso te ajudar"
+    ];
+    
+    var xingamento = [
+            "**" + textoMsg + "\**??? Epa! Olha a boca, rapaz!...",
+            "**" + textoMsg + "\**??? Sua mãe não te deu educação, não?",
+            "**" + textoMsg + "\**??? Seu IP foi registrado. Vou mandar o Robocop aí te dar uma lição."
+            ];
+    
+    var Default = [
+            
+            'Não entendi o que vc quis dizer com **' + textoMsg +'\** . Minha especialidade é responder sobre realidade aumentada e virtual' ,
+            ' Poutz! Não entendi o que vc quis dizer com **'+ textoMsg +'\**',
+            'Eita... agora vc me pegou. Ainda não aprendi como responder a **'+ textoMsg +'\** '
+            ];
+
+}
+
+module.exports = Textos;
